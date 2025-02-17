77b131e440e15a802871cb851eb3befad14ea888
Jere <jereenunez@gmail.com>
2025-02-17 16:21:07 -0300

    Se creo la UI de la post page y proteccion como administrador

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function DashboardPage  () {
+
+        return (
+            <h1 className="text-center text-3xl my-7 font-semibold">
+                DashBoardPage
+            </h1>
+        )
+    }
