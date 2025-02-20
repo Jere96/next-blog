fb8d306d8c89430dc421fe53f1449250f9f0ae11
Jere <jereenunez@gmail.com>
2025-02-20 17:42:04 -0300

    Se completa la pagina del perfil en el panel de usuario (Dashboard)

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -70,6 +70,7 @@ export default function Header() {
             appearance={{
               baseTheme: theme === "light" ? light : dark,
             }}
+            userProfileUrl='/dashboard?tab=profile'
           />
         </SignedIn>
         <SignedOut>
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,38 @@
 "use client";
 
-export default function DashboardPage  () {
+import { useEffect, useState } from "react";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import { useSearchParams } from "next/navigation";
+import DashPosts from "../components/DashPosts";
+import DashUsers from "../components/DashUsers";
+import DashboardComp from "../components/DashboardComp";
 
-        return (
-            <h1 className="text-center text-3xl my-7 font-semibold">
-                DashBoardPage
-            </h1>
-        )
+export default function Dashboard() {
+  const searchParams = useSearchParams();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(searchParams);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
     }
+  }, [searchParams]);
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === "profile" && <DashProfile />}
+
+      {tab === "posts" && <DashPosts />}
+
+      {tab === "users" && <DashUsers />}
+      {tab === "dash" && <DashboardComp />}
+    </div>
+  );
+}
